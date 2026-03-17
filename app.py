e0c8a051378e547feff5fda85150c83abcf172fc
User <user@MacBook-Pro-User.local>
2026-03-17 15:00:22 +0300

    remove flask run for render

--- app.py
+++ app.py
@@ -324,8 +324,3 @@ def payouts():
 
     return render_template("payouts.html",user=user)
 
-import os
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
