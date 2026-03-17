405c825619d5f37504500d8426f9ca6253bd5996
User <user@MacBook-Pro-User.local>
2026-03-17 14:56:56 +0300

    fix final render startup

--- app.py
+++ app.py
@@ -324,7 +324,8 @@ def payouts():
 
     return render_template("payouts.html",user=user)
 
+import os
 
-app.run(debug=True)
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
